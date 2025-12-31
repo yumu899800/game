eb54530b92edc4c0627e669e946afe45b7a66867
Xipho19 <1959474407@qq.com>
2025-12-31 10:31:39 +0800

    tubiao

---
+++ assets/scripts/Item.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Item')
+export class Item extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
